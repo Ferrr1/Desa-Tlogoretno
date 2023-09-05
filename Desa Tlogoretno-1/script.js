ce629ac1f69af450d4f21944b434e680dff20a7a
Maulana Feri Setyawan <maulanasetyawan8@gmail.com>
2023-09-05 21:36:24 +0700

    Desa Tlogoretno-1

--- Desa Tlogoretno-1/script.js
+++ Desa Tlogoretno-1/script.js
@@ -21,15 +21,6 @@ buttons.forEach(button => {
   })
 })
 
-searchBox.addEventListener("click", () => {
-  navbar.classList.toggle("showInput");
-  if (navbar.classList.contains("showInput")) {
-    searchBox.classList.replace("bx-search", "bx-x");
-  } else {
-    searchBox.classList.replace("bx-x", "bx-search");
-  }
-});
-
 // sidebar open close js code
 let navLinks = document.querySelector(".nav-links");
 let menuOpenBtn = document.querySelector(".navbar .bx-menu");
